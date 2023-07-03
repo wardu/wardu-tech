b8cfc629dd8b505768227de53d3ddd62632d4a0c
Warren Dubery <warrendubery@protonmail.ch>
2023-07-03 14:44:00 +0100

    :construction: start working together section

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import Who from "@/components/Who/Who";
 import Questions from "@/components/Questions/Questions";
 import Process from "@/components/Process/Process";
 import Updates from "@/components/Updates/Updates";
+import WorkTogether from "@/components/WorkTogether/WorkTogether";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <Questions />
       <Process />
       <Updates />
+      <WorkTogether />
     </>
   );
 }
