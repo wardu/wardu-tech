8f9530253582f4f3a6016a731a1e28af0720cc87
Warren Dubery <warrendubery@protonmail.ch>
2023-07-03 10:37:28 +0100

    :construction: add content

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import MeetFounder from "@/components/MeetFounder/MeetFounder";
 import Who from "@/components/Who/Who";
 import Questions from "@/components/Questions/Questions";
 import Process from "@/components/Process/Process";
+import Updates from "@/components/Updates/Updates";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       <Who />
       <Questions />
       <Process />
+      <Updates />
     </>
   );
 }
