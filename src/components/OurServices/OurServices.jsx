ece074c30f24271dd6a53feb1f15c491b60ac49d
Warren Dubery <warrendubery@protonmail.ch>
2023-07-17 16:27:25 +0100

    :speech_balloon: reword services copy

--- src/components/OurServices/OurServices.jsx
+++ src/components/OurServices/OurServices.jsx
@@ -23,25 +23,30 @@ function Card({ image, title, text }) {
 const OurServices = () => {
   const cards = [
     {
-      image: "/images/services/web-dev-laptop.svg",
-      title: "Web Development",
-      text: `We create professional and great looking websites that enhance your customer’s experience.
-      Embracing a mobile-first development approach is vital in today's digital age. As mobile devices have become the primary tool for accessing the internet, it's essential to design web assets that not only look impressive but also deliver exceptional performance on mobile platforms.`,
-    },
-    {
       image: "/images/services/responsive-tablet.svg",
       title: "Fully Responsive Design",
-      text: `Embracing a mobile-first development approach is vital in today's digital age. As mobile devices have become the primary tool for accessing the internet, it's essential to design web assets that not only look impressive but also deliver exceptional performance on mobile platforms. Enhance your customer experience with optimised, mobile-friendly designs.`,
+      text: `We use a mobile-first development approach. This is essential. Not only did Google decide to rank websites by their mobile performance over desktop performance since 2018, but also 60%+ of web traffic comes from mobile.
+      Mobile-friendly designs are great for SEO and the user experience.
+      We make sure your website looks great on all devices.`,
+    },
+    {
+      image: "/images/services/web-dev-laptop.svg",
+      title: "Web Development",
+      text: `All of our websites are custom and bespoke.
+      We use the same technology that underpins the world's most performant websites, such as Uber, Starbucks, OpenAI (ChatGPT), GoDaddy, and Netflix.
+      
+      Your high-performing website will enhance the customer experience and help you rank higher on Google.
+     `,
     },
     {
       image: "/images/services/seo-graph.svg",
       title: "SEO & Analytics",
-      text: "comprehensive SEO services designed to elevate your business's online presence. We understand that visibility is key in the digital age, and our team of SEO experts is committed to optimizing your website for search engines. We employ a variety of strategies, including keyword research, on-page and off-page optimization, and content creation, all tailored to your specific industry and target audience. Our goal is to improve your website's ranking, increase organic traffic, and ultimately drive more conversions. With our SEO services, we help your business stand out in the crowded digital landscape.",
+      text: "We optimise your website for search engines using a variety of strategies, including , loading optimisation, keyword research and content creation, all tailored to your specific industry and target audience. Our goal is to improve your website's ranking, increase organic traffic, driving more conversions. We also provide you with detailed analytics reports, so you can see how your website is performing and make informed decisions about your marketing strategy.",
     },
     {
       image: "/images/services/maintenance-cloud.svg",
       title: "Continued Support & Maintenance",
-      text: "We give you comprehensive maintenance and support services designed to keep your systems running smoothly. Our dedicated team of experts is available around the clock to troubleshoot issues, perform regular system checks, and implement updates as needed. We understand that downtime can be costly, so we strive to resolve issues swiftly and efficiently. Our proactive approach to maintenance helps prevent potential problems, ensuring your operations continue uninterrupted. With our support, you can focus on what you do best - running your business.",
+      text: "Our all-inclusive maintenance and support services are designed to ensure your website runs smoothly. We are committed to quick and effective problem resolution with a guarantee of fixing any critical issues on the same day. With our backing, you can concentrate on your primary task - managing your business.",
     },
   ];
 
