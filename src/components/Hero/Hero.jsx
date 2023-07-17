75d76929c36afbb3d642e8e04464cb1c655a918c
Warren Dubery <warrendubery@protonmail.ch>
2023-07-17 14:45:15 +0100

    :speech_balloon: change Hero content

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,34 +5,14 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.heroImage}>
-        <Image
-          priority={true}
-          src={"/images/blurBall.png"}
-          fill={true}
-          alt='blur ball'
-          quality={100}
-          sizes='100vw'
-        />
-      </div>
       <div classname={styles.outter}>
         <div className={styles.container}>
           <div className={styles.left}>
             <div className={styles.titleWrapper}>
-              <h1 className={styles.topTitle}>High performance</h1>
-              <h1 className={styles.lowerTitle}>websites and designs</h1>
+              <h1 className={styles.topTitle}>Giving Super Powers</h1>
+              <h1 className={styles.lowerTitle}>to Small Websites</h1>
             </div>
-            <div className={styles.scrollContainer}>
-              <Image
-                src={"/arrow.png"}
-                width={10}
-                height={70}
-                alt='scroll arrow'
-              />
-              <h3 className={styles.scrollText}>
-                SCROLL DOWN TO FIND OUT MORE
-              </h3>
-            </div>
+
             <h3 className={styles.marketLeaders}>
               MARKET LEADERS USE WARDU TECHNOLOGIES TO GROW
             </h3>
@@ -42,13 +22,17 @@ const Hero = () => {
             <div className={styles.rightContainer}>
               <div className={styles.heroTextWrapper}>
                 <p className={styles.heroText}>
-                  Wardu Technologies is a web development powerhouse based in
+                  Wardu Technologies is a web development agency based in
                   London, UK.
                 </p>
                 <p className={styles.heroText}>
-                  We craft high-performance, bespoke websites and web
-                  applications. Our websites ensure blazing speed, captivating
-                  designs, and enhanced search rankings.
+                  Our bespoke websites are never from template. We use
+                  state-of-the-art technologies to ensure the fastest loading
+                  speeds possible.
+                </p>
+                <p className={styles.heroText}>
+                  Our approach is particularly beneficial for the smaller
+                  websites competing in a world of giants.
                 </p>
               </div>
               <div className={styles.button}>
@@ -57,48 +41,48 @@ const Hero = () => {
                   text='Work With Us &nbsp;&nbsp;&nbsp;&nbsp;  →'
                 />
               </div>
-              <div className={styles.brandsContainer}>
-                <p className={styles.brandsTitle}>Brands we work with:</p>
-                <div className={styles.brands}>
-                  <Image
-                    src={"/images/brands/amazon-logo.svg"}
-                    width={112}
-                    height={33}
-                    alt='amazon logo'
-                  />
-                  <Image
-                    src={"/images/brands/node-logo.svg"}
-                    width={90}
-                    height={90}
-                    alt='node logo'
-                  />
-                  <Image
-                    src={"/images/brands/react-logo.svg"}
-                    width={55}
-                    height={55}
-                    alt='react logo'
-                  />
-                  <Image
-                    src={"/images/brands/google-logo.svg"}
-                    width={147}
-                    height={31}
-                    alt='google analytics logo'
-                  />
-                  <Image
-                    src={"/images/brands/vercel-logo.svg"}
-                    width={150}
-                    height={140}
-                    alt='vercel logo'
-                  />
-                  <Image
-                    src={"/images/brands/github-logo.svg"}
-                    width={158}
-                    height={52}
-                    alt='github logo'
-                  />
-                </div>
-              </div>
             </div>
+          </div>
+        </div>
+        <div className={styles.brandsContainer}>
+          <p className={styles.brandsTitle}>We work with:</p>
+          <div className={styles.brands}>
+            <Image
+              src={"/images/brands/amazon-logo.svg"}
+              width={112}
+              height={33}
+              alt='amazon logo'
+            />
+            <Image
+              src={"/images/brands/node-logo.svg"}
+              width={90}
+              height={90}
+              alt='node logo'
+            />
+            <Image
+              src={"/images/brands/react-logo.svg"}
+              width={55}
+              height={55}
+              alt='react logo'
+            />
+            <Image
+              src={"/images/brands/google-logo.svg"}
+              width={147}
+              height={31}
+              alt='google analytics logo'
+            />
+            <Image
+              src={"/images/brands/vercel-logo.svg"}
+              width={150}
+              height={140}
+              alt='vercel logo'
+            />
+            <Image
+              src={"/images/brands/github-logo.svg"}
+              width={158}
+              height={52}
+              alt='github logo'
+            />
           </div>
         </div>
       </div>
