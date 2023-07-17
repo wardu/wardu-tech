e1c65536eaeb8bc0f27a159a287eb7eb2e37b072
Warren Dubery <warrendubery@protonmail.ch>
2023-07-17 19:03:41 +0100

    :coffin: remove unused code

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,4 @@
 import Hero from "@/components/Hero/Hero";
-import Intro from "@/components/intro/Intro";
-import Carousel from "@/components/carousel/Carousel";
-import MeetFounder from "@/components/MeetFounder/MeetFounder";
-import Who from "@/components/Who/Who";
-import Questions from "@/components/Questions/Questions";
-import Process from "@/components/Process/Process";
-import Updates from "@/components/Updates/Updates";
-import WorkTogether from "@/components/WorkTogether/WorkTogether";
 import Testimonials from "@/components/Testimonials/Testimonials";
 import data from "@/components/Testimonials/data.json";
 import AboutUs from "@/components/AboutUs/AboutUs";
@@ -21,14 +13,6 @@ export default function Home() {
       <AboutUs />
       <OurServices />
       <OurWork />
-      {/* <Intro /> */}
-      {/* <Carousel /> */}
-      {/* <MeetFounder /> */}
-      {/* <Who /> */}
-      {/* <Questions /> */}
-      {/* <Process /> */}
-      {/* <Updates /> */}
-      {/* <WorkTogether /> */}
       <Testimonials testimonialData={data} />
       <FinalCTA />
     </>
