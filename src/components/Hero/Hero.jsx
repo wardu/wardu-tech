8c5d73da10d77c0e0f144da739be1020f37ce8c9
Warren Dubery <warrendubery@protonmail.ch>
2023-07-21 10:46:26 +0100

    :lipstick: restyle Hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,35 +6,30 @@ const Hero = () => {
   return (
     <div className={styles.container}>
       <div classname={styles.outter}>
+        <div className={styles.titleWrapper}>
+          <h1 className={styles.topTitle}>Give Your Website</h1>
+          <h1 className={styles.lowerTitle}>Super Powers</h1>
+        </div>
         <div className={styles.container}>
           <div className={styles.left}>
-            <div className={styles.titleWrapper}>
-              <h1 className={styles.topTitle}>Giving Super Powers</h1>
-              <h1 className={styles.lowerTitle}>to Small Websites</h1>
+            <div className={styles.heroTextWrapper}>
+              <p className={styles.heroText}>
+                Wardu Technologies is a web development agency based in London,
+                UK.
+              </p>
+              <p className={styles.heroText}>
+                We make bespoke websites using state-of-the-art technologies to
+                ensure the fastest loading speeds possible.
+              </p>
+              <p className={styles.heroText}>
+                Our approach is particularly beneficial for the smaller
+                businesses competing in a world of giants.
+              </p>
             </div>
-
-            <h3 className={styles.marketLeaders}>
-              MARKET LEADERS USE WARDU TECHNOLOGIES TO GROW
-            </h3>
           </div>
 
           <div className={styles.right}>
             <div className={styles.rightContainer}>
-              <div className={styles.heroTextWrapper}>
-                <p className={styles.heroText}>
-                  Wardu Technologies is a web development agency based in
-                  London, UK.
-                </p>
-                <p className={styles.heroText}>
-                  Our bespoke websites are never from template. We use
-                  state-of-the-art technologies to ensure the fastest loading
-                  speeds possible.
-                </p>
-                <p className={styles.heroText}>
-                  Our approach is particularly beneficial for the smaller
-                  websites competing in a world of giants.
-                </p>
-              </div>
               <div className={styles.button}>
                 <Button
                   url='/contact'
